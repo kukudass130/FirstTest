7b943dee3ac8d36700cc31910426e2e69f5183d8
kukudass130 <tldhswls123@naver.com>
2023-08-02 10:59:12 +0900

    Fanal commit

--- Python_practice/First.py
+++
@@ -1,2 +0,0 @@
-for i in range(1, 11):
-    print(str(i) + '번째 인사') #10번 인사하는 반복문
--- Python_practice/Second_calculator.py
+++
@@ -1,5 +0,0 @@
-for i in range(100):
-    if i%7 ==0:
-        print(i)
-a = round((50+40+45+67+34)/5, 1)
-print(a)
